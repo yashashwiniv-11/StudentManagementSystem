1196e41d50a689f15bcfeec810804af32d0805ec
Yashashwini <veerabommay@gmail.com>
2026-03-01 20:32:55 +0530

    Final version - Student Management System with CRUD operations

--- Main.java
+++ Main.java
@@ -1,38 +1,49 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        Student[] students = new Student[10];
-        int count = 0;
+        ArrayList<Student> students = new ArrayList<>();
 
         while (true) {
+
             System.out.println("\n1. Add Student");
             System.out.println("2. Display Students");
             System.out.println("3. Search Student");
             System.out.println("4. Delete Student");
-            System.out.println("5. Exit");
+            System.out.println("5. Update Marks");
             System.out.println("6. Exit");
             System.out.print("Choose option: ");
+
             int choice = sc.nextInt();
             sc.nextLine();
 
             if (choice == 1) {
                 System.out.print("Enter name: ");
                 String name = sc.nextLine();
+
                 System.out.print("Enter marks: ");
                 int marks = sc.nextInt();
-                students[count++] = new Student(name, marks);
+
+                students.add(new Student(name, marks));
                 System.out.println("Student Added!");
-            } 
+            }
+
             else if (choice == 2) {
-                for (int i = 0; i < count; i++) {
-                    students[i].display();
+                if (students.isEmpty()) {
+                    System.out.println("No students available!");
+                } else {
+                    for (Student s : students) {
+                        System.out.println(s);
+                    }
                 }
-            } 
-            else if (choice==3){
-                System.out.print("Enter name to search:");
-                String searchName = sc.next();
+            }
+
+            else if (choice == 3) {
+                System.out.print("Enter name to search: ");
+                String searchName = sc.nextLine();
                 boolean found = false;
 
                 for (Student s : students) {
@@ -46,35 +57,58 @@ public class Main {
                 if (!found) {
                     System.out.println("Student Not Found!");
                 }
-                break;
             }
-            else if (choice==4){
+
+            else if (choice == 4) {
                 System.out.print("Enter name to delete: ");
-                String deleteName = sc.next();
-                students.removeIf(s -> s.getName().equalsIgnoreCase(deleteName));
-                System.out.println("Student deleted (if existed).");
-                break;
+                String deleteName = sc.nextLine();
+                boolean deleted = false;
+
+                for (int i = 0; i < students.size(); i++) {
+                    if (students.get(i).getName().equalsIgnoreCase(deleteName)) {
+                        students.remove(i);
+                        deleted = true;
+                        System.out.println("Student Deleted!");
+                        break;
+                    }
+                }
+
+                if (!deleted) {
+                    System.out.println("Student Not Found!");
+                }
             }
-            else if (choice==5){
+
+            else if (choice == 5) {
                 System.out.print("Enter name to update: ");
-                String updateName = sc.next();
+                String updateName = sc.nextLine();
+                boolean updated = false;
+
                 for (Student s : students) {
                     if (s.getName().equalsIgnoreCase(updateName)) {
                         System.out.print("Enter new marks: ");
                         int newMarks = sc.nextInt();
                         s.setMarks(newMarks);
-                        System.out.println("Marks updated!");
+                        updated = true;
+                        System.out.println("Marks Updated!");
+                        break;
                     }
                 }
-            break;
+
+                if (!updated) {
+                    System.out.println("Student Not Found!");
+                }
             }
+
             else if (choice == 6) {
                 System.out.println("Exiting...");
                 break;
-            } 
+            }
+
             else {
-                System.out.println("Invalid option");
+                System.out.println("Invalid option!");
             }
         }
+
+        sc.close();
     }
 }
--- Student.java
+++ Student.java
@@ -1,13 +1,25 @@
 public class Student {
-    String name;
-    int marks;
+    private String name;
+    private int marks;
 
     public Student(String name, int marks) {
         this.name = name;
         this.marks = marks;
     }
 
-    public void display() {
-        System.out.println("Name: " + name + ", Marks: " + marks);
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Marks: " + marks;
     }
 }
